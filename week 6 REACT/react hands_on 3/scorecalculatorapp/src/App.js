72826ad43c39c8cdd630cfe5d3a26292b1596d2c
kandlaguntapravallika <kandlaguntapravallika@gmail.com>
2025-07-27 16:14:04 +0530

    Add files via upload

---
+++ week 6 REACT/react hands_on 3/scorecalculatorapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div className="App">
+      <CalculateScore name="John Doe" school="Greenwood High" total={450} goal={5} />
+    </div>
+  );
+}
+
+export default App;
